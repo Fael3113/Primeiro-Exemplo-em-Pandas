a1555e7ce514e15075f0ae9ff932362cec7db9a5
Fael3113 <santiagorafaelsantiago@hotmail.com>
2023-02-06 22:07:07 -0300

    Create Primeiro Exemplo em Pandas.py

---
+++ Primeiro Exemplo em Pandas.py
@@ -0,0 +1,6 @@
+#Exemplo de Manipulação de dados usando Python Pandas 
+# 6/02/2023 
+# Autor:Poyatos e galera
+import pandas as pd
+
+combustiveis_df = pd.read_csv("precos-semestrais-ca-2022-01.csv")
